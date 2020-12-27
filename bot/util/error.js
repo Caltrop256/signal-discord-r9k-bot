b1c7c6a16982e7611603fe63b2005eb579065d5f
Caltrop <caltrop@caltrop.dev>
2020-12-27 19:41:17 +0100

    update error message

--- bot/util/error.js
+++ bot/util/error.js
@@ -3,7 +3,7 @@ module.exports = {
     handler: function(output, userId, guildId, timestamp, error = {}) {
         console.error(`\x1b[31m[${error.name || 'ERROR'}] \x1b[35m[${new Date(timestamp).toLocaleString()}]\x1b[0m : \x1b[36m"${error.message || 'Poorly structured error'}"\x1b[0m ; list[${client.error.list.length}]`);
         client.error.list.push({userId, guildId, timestamp, error});
-        if(output) output.send(client.embed.error('An Error occured while trying to process your request.\n\nConsider reporting this error here'));
+        if(output) output.send(client.embed.error('An Error occured while trying to process your request.\n\nConsider reporting this error [here](https://github.com/Caltrop256/signal-discord-r9k-bot/issues/new?assignees=&labels=&template=bug_report.md&title=)!'));
     },
     list: []
 }
